1aa1cd087144f788aa65e30dbf1c0fb3615f7ec8
unknown <cnahass@gmail.com>
2026-09-03 18:51:30 +0100

    Initial commit

---
+++ plateforme-admin/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ return (
+   <div className="font-bold text-3xl text-green-600">
+    PlateForme Admin
+    <Button>Click Me</Button>
+   </div> 
+  )
+}
+
+export default App
---
+++ plateforme-admin/vite.config.ts
@@ -0,0 +1,23 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  server:{
+    port:3002,
+    strictPort:true,
+  },
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
